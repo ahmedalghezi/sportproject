0b5708d0a3a2026a31f6a68c078bbdd4efea0f20
minisidi <83781799+minisidi@users.noreply.github.com>
2022-07-13 13:49:06 +0200

    added footer component

---
+++ src/register/footer.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="footer">
+        <div id="beside">
+          <div className="ldi">
+            <a href="https://www.inprove.info/" target="_blank">
+              {" "}
+              <img src="https://www.inprove.info/wp-content/uploads/2022/03/inprove_logo_weiss-200x52.png" />
+            </a>
+          </div>{" "}
+          <div className="ldi">
+            <p>
+              © <span className="numbers">2022</span>&nbsp;in:prove
+              <br />
+              <a href="https://www.inprove.info/datenschutz/">
+                Daten­schutz
+              </a> | <a href="https://www.inprove.info/impressum/">Impressum</a>
+            </p>
+          </div>
+          <div className="icondiv">
+            <link
+              rel="stylesheet"
+              href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+              integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+              crossorigin="anonymous"
+              referrerpolicy="no-referrer"
+            />
+            <div className="container">
+              <p>
+                <a
+                  className="icon"
+                  href="https://twitter.com/inprove_info"
+                  target="_blank"
+                >
+                  <i className="fab fa-twitter"></i>
+                </a>
+                <a
+                  className="icon"
+                  href="https://www.instagram.com/inprove.info/"
+                  target="_blank"
+                >
+                  <i className="fab fa-instagram"></i>
+                </a>
+                <a
+                  className="icon"
+                  href="https://www.linkedin.com/company/inprove-info"
+                  target="_blank"
+                >
+                  <i className="fab fa-linkedin"></i>
+                </a>
+              </p>
+            </div>{" "}
+          </div>
+          <div className="bisp">
+            <a
+              href="https://www.bisp.de"
+              target="_blank"
+              aria-label="Gefoerdert_durch_BISp_rgb"
+              rel="noopener noreferrer"
+            >
+              <img
+                width="100"
+                height="90"
+                src="https://www.inprove.info/wp-content/uploads/2022/04/Gefoerdert_durch_BISp_rgb.png"
+                data-orig-src="https://www.inprove.info/wp-content/uploads/2022/04/Gefoerdert_durch_BISp_rgb.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
+
+//###############################################################################
+/*   */
--- src/register/navBar.js
+++ src/register/navBar.js
@@ -6,6 +6,7 @@ import React, { Component } from "react";
 import PostSignup from "../DB/postSignup";
 import { Link } from "react-router-dom";
 import image from "../images/inprove_logo-400x103.png";
+import Footer from "./footer";
 
 export default class NavBar extends Component {
   constructor(props) {
@@ -75,7 +76,7 @@ export default class NavBar extends Component {
                   hidden={!this.state.showSignIn /*|| this.props.loggedin*/}
                 >
                   <Link className="nav-link" to={"/reg/sign-in"}>
-                    Sign in
+                    Login
                   </Link>
                 </li>
 
@@ -87,7 +88,7 @@ export default class NavBar extends Component {
 
                 <li className="nav-item" hidden={!this.state.showSignUp}>
                   <Link className="nav-link" to={"/reg/sign-up-van"}>
-                    Sign up
+                    Registrieren
                   </Link>
                 </li>
 
@@ -149,76 +150,7 @@ export default class NavBar extends Component {
           </div>
         </nav>
 
-        <div className="footer">
-          <div id="beside">
-            <div className="ldi">
-              <a href="https://www.inprove.info/" target="_blank">
-                {" "}
-                <img src="https://www.inprove.info/wp-content/uploads/2022/03/inprove_logo_weiss-200x52.png" />
-              </a>
-            </div>{" "}
-            <div className="ldi">
-              <p>
-                © <span className="numbers">2022</span>&nbsp;in:prove
-                <br />
-                <a href="https://www.inprove.info/datenschutz/">
-                  Daten­schutz
-                </a>{" "}
-                | <a href="https://www.inprove.info/impressum/">Impressum</a>
-              </p>
-            </div>
-            <div className="icondiv">
-              <link
-                rel="stylesheet"
-                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
-                integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
-                crossorigin="anonymous"
-                referrerpolicy="no-referrer"
-              />
-              <div className="container">
-                <p>
-                  <a
-                    className="icon"
-                    href="https://twitter.com/inprove_info"
-                    target="_blank"
-                  >
-                    <i className="fab fa-twitter"></i>
-                  </a>
-                  <a
-                    className="icon"
-                    href="https://www.instagram.com/inprove.info/"
-                    target="_blank"
-                  >
-                    <i className="fab fa-instagram"></i>
-                  </a>
-                  <a
-                    className="icon"
-                    href="https://www.linkedin.com/company/inprove-info"
-                    target="_blank"
-                  >
-                    <i className="fab fa-linkedin"></i>
-                  </a>
-                </p>
-              </div>{" "}
-            </div>
-            <div className="bisp">
-              <a
-                href="https://www.bisp.de"
-                target="_blank"
-                aria-label="Gefoerdert_durch_BISp_rgb"
-                rel="noopener noreferrer"
-              >
-                <img
-                  width="100"
-                  height="90"
-                  src="https://www.inprove.info/wp-content/uploads/2022/04/Gefoerdert_durch_BISp_rgb.png"
-                  data-orig-src="https://www.inprove.info/wp-content/uploads/2022/04/Gefoerdert_durch_BISp_rgb.png"
-                  alt=""
-                />
-              </a>
-            </div>
-          </div>
-        </div>
+        <Footer />
       </div>
     );
   }
