41807d515d0ee10cff5bdd62e8c3c54f478acc6e
Nicolas Schulz <nicolas.schulz@web.de>
2023-01-16 15:35:41 +0100

    added download coach data

---
+++ src/nico/downloadCoachData/EvaluationsView.js
@@ -0,0 +1,122 @@
+import React from 'react'
+import CustomChart from '../../utli/components/CustomChart';
+import CustomTable from '../../utli/components/CustomTable';
+import FilterFunction from '../../utli/components/FilterFunction';
+import { evaluationsHeadCells } from '../../temp-data/evaluations';
+import axios from "axios";
+import { reformatDate } from '../../utli/dataConversion';
+import { germanDatePresentation } from "../../utli/dataConversion";
+import HandelTrainer from "../../DB/handelTrainer";
+//TODO change order of table columns to: Datum-Name-Titel-Qualität-Geplante Int.-Erreichte Int.-Differenz
+//TODO German date format
+const tableCellStyle = {width: '25%', paddingBottom: '8px'};
+
+async function getEvaluations () {
+  return await axios.create({
+     baseURL: "https://inprove-sport.info",
+     json: true,
+     headers: {
+         "Content-type": "application/json"
+     },
+  }).get("/trainer/getHistory");
+}
+function downloadCoach(chartAthlete) {
+  HandelTrainer.disguisedTrainerLogin({trainerID: chartAthlete}).then(response => {
+      if (response.data.res === "ok") {
+
+      } else {
+          alert("Es ist ein Fehler aufgetreten!");
+      }
+  }).catch(e => {
+      console.log(e);
+      alert("Es ist ein Fehler aufgetreten!");
+  });
+}
+
+export default function EvaluationsView() {
+  const [isChartView, setIsChartView] = React.useState(false);
+  const [chartAthlete, setChartAthlete] = React.useState(false);
+  const [evaluations, setEvaluations] = React.useState([]);
+  const [filteredEvaluations, setFilterEvaluations] = React.useState([]);
+
+  if(evaluations.length === 0) {
+    getEvaluations().then(res => {
+      const athletesData = res['data']['athletes'];
+      if(!athletesData || res['data']['res'] != "ok")
+        return;
+      setEvaluations(athletesData);
+      setFilterEvaluations(athletesData);
+    });
+  }
+
+  
+
+  // data preprocessing
+  const titles = Array.from(new Set(evaluations.map(el => el.title)));
+  const athletes = Array.from(new Set(evaluations.map(el => el.name)));
+
+  return (
+    <>
+    <div className="view-header">
+      <div>
+        <FilterFunction 
+          initialEvaluations={evaluations}
+          titles={titles} 
+          athletes={athletes}
+          updateEvaluations={data => {
+            setFilterEvaluations(data);
+          }}
+        />
+      </div>
+    </div>
+    <div className="view-content">
+      {isChartView ? 
+        <CustomChart 
+          evaluations={filteredEvaluations.filter(el => el.name === chartAthlete)} 
+          headCells={evaluationsHeadCells}
+          selectedAthlete={chartAthlete}
+          closeChartView={() => {
+            setChartAthlete(false);
+            setIsChartView(false);
+          }}
+          />
+        : <CustomTable
+              rows={filteredEvaluations} 
+              headCells={evaluationsHeadCells}
+              toggleChartView={athlete => {
+                setChartAthlete(athlete);
+                setIsChartView(!isChartView);
+              }} 
+              title={''}
+              hasSpecialRow={true}
+              hasChartRepresentation={true}
+              dense={false}
+              statsSection={
+                  <table  style={{margin: '0 18px 32px'}}>
+                    <tbody>
+                      <tr>
+                          <td style={tableCellStyle}><b>Trainingseinheiten diesen Monat:</b></td>
+                          <td style={tableCellStyle}>{evaluations.length}</td>
+                      </tr>
+                      <tr>
+                          <td style={tableCellStyle}><b>Anzahl Athlet*innen:</b></td>
+                          <td style={tableCellStyle}>{Array.from(new Set(evaluations.map(ev => ev.name))).length}</td>
+                      </tr>
+                      <tr>
+                          <td style={tableCellStyle}><b>Letzte Trainingseinheit:</b></td>
+                          <td style={tableCellStyle}>{reformatDate(Array.from(new Set(evaluations.map(ev => ev.date))).sort().reverse()[0]) }</td>
+                          
+                          
+                          
+                      </tr>
+                    </tbody>
+                  </table>
+                }
+                rowsPerPage={10}
+              />}
+        
+    </div>
+    <div className="view-footer"><button className={"btn btn-primary btn-block"} onClick={downloadCoach}> Download Coachdata</button></div></>
+  )
+}
+// 
---
+++ src/nico/downloadCoachData/editCoach.js
@@ -0,0 +1,183 @@
+/*
+By Ahmed Al-Ghezi
+ */
+
+import React, {Component} from "react";
+import HandelTrainer from "../../DB/handelTrainer";
+import PostSignup from "../../DB/postSignup";
+
+export default class EditCoach extends Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {trainersList: [], selectedTrainer: '', email: '', preEvArr: [], selectedTrainerID: undefined};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleTrainersListClick = this.handleTrainersListClick.bind(this);
+        this.handleDisguisedLogin = this.handleDisguisedLogin.bind(this);
+        this.getTrainers = this.getTrainers.bind(this);
+        this.makeCoach = this.makeCoach.bind(this);
+        this.downloadCoach = this.downloadCoach.bind(this);
+    }
+
+
+    componentDidMount() {
+        this.getTrainers();
+    }
+
+    getTrainers() {
+        HandelTrainer.getAllTrainers().then(response => {
+            if (response.data.res === "error") {
+                const arr = ["connection error"];
+                this.setState({trainersList: arr});
+                return;
+            }
+            if (response.data.res === "error") {
+                alert("Bitte erst anmelden.");
+                return;
+            }
+            if (response.data.res === "ok") {
+                this.setState({trainersList: response.data.trainersList});
+            }
+
+        }).catch(e => {
+            console.log(e);
+            alert("Es ist ein Fehler aufgetreten!");
+        });
+    }
+
+    makeCoach(event) {
+        event.preventDefault();
+        HandelTrainer.makeCoach({email: this.state.email}).then(response => {
+            if (response.data.res === "error") {
+                alert("Es ist ein Fehler aufgetreten!");
+                return;
+            }
+            if (response.data.res === "ok") {
+                alert("Trainer korrekt erstellt!");
+                this.setState({email: ''});
+                this.getTrainers();
+                return;
+            }
+
+        }).catch(e => {
+            console.log(e);
+            alert("Es ist ein Fehler aufgetreten!");
+        });
+    }
+
+    handleTrainersListClick(event) {
+        event.preventDefault();
+
+        if (this.state.preEvArr['trainerList']) {
+            this.state.preEvArr['trainerList'].target.classList.remove("active");
+        }
+        event.target.classList.add("active");
+        // this.state.preEvArr['testList'] = event;
+
+        const arr = this.state.preEvArr;
+        arr['trainerList'] = event;
+        this.setState({preEvArr: arr});
+
+        this.setState({selectedTrainer: event.target.name});
+        this.setState({selectedTrainerID: event.target.ID});
+
+    }
+
+    handleDisguisedLogin(event) {
+        event.preventDefault();
+        if (this.state.selectedTrainer === '') {
+            alert("Bitte Trainer*in auswählen.");
+            return;
+        }
+        PostSignup.disguisedTrainerLogin({email: this.state.selectedTrainer}).then(response => {
+            if (response.data.res === "ok") {
+                //this.props.navigate('/trainer/addAthletes');
+                window.location.href = window.location.origin + "/trainer/addAthletes";
+            } else {
+                alert("Es ist ein Fehler aufgetreten!");
+            }
+        }).catch(e => {
+            console.log(e);
+            alert("Es ist ein Fehler aufgetreten!");
+        });
+    }
+
+    downloadCoach(event) {
+        event.preventDefault();
+        if (this.state.selectedTrainer === '') {
+            alert("Bitte Trainer*in auswählen.");
+            return;
+        }
+        PostSignup.disguisedTrainerLogin({trainerID: this.state.selectedTrainerID}).then(response => {
+            if (response.data.res === "ok") {
+
+            } else {
+                alert("Es ist ein Fehler aufgetreten!");
+            }
+        }).catch(e => {
+            console.log(e);
+            alert("Es ist ein Fehler aufgetreten!");
+        });
+    }
+
+
+    handleChange(event) {
+        const target = event.target;
+        let value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    editGroups = (event) =>  {
+        event.preventDefault();
+        window.location.href = window.location.origin + "/trainer/createGroup";
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleDisguisedLogin}>
+                <h3>Trainer*in erstellen & bearbeiten</h3>
+
+                <div className="form-group">
+                    <input type="email" className="form-control" placeholder="Email eingeben " name="email"
+                           onChange={this.handleChange}/>
+                </div>
+                <button onClick={this.makeCoach} className="btn btn-secondary btn-block" disabled={false}>erstelle
+                    Trainer*in
+                </button>
+
+                <br></br><br></br>
+
+                <table>
+                    <tr>
+                        <td>
+                            <div className="vertical-menu">
+                                <a href="src/trainer#" className="active">Trainer*innen</a>
+                                {this.state.trainersList.map((option) => (
+                                    <a name={option.email} key={option.email}
+                                       onClick={this.handleTrainersListClick}>{option.name}</a>
+                                ))}
+                            </div>
+                        </td>
+
+                        <td>
+                            &nbsp;&nbsp;&nbsp;&nbsp;<button className={"btn btn-primary btn-block"} onClick={this.editGroups}> Edit Groups</button>
+                            &nbsp;&nbsp;&nbsp;&nbsp;<button className={"btn btn-primary btn-block"} onClick={this.downloadCoach}> Download Coachdata</button>
+                        </td>
+                        
+                    </tr>
+                    
+                </table>
+
+
+                <button type="submit" className="btn btn-primary btn-block">als Trainer*in anmelden</button>
+            </form>
+        );
+    }
+
+
+
+}
