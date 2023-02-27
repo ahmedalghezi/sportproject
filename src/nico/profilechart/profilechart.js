039fecf0bdc802be3c3eb6ab2222ca7ad174c3f1
Nicolas Schulz <nicolas.schulz@web.de>
2023-02-27 12:41:13 +0100

    added profilegrid to display multiple charts

--- src/nico/profilechart/profilechart.js
+++ src/nico/profilechart/profilechart.js
@@ -6,84 +6,39 @@ import React, {Component} from "react";
 import handelTrainer from "../../DB/handelTrainer";
 import Plot from 'react-plotly.js';
 
-const testdata = [
-    {value: 27.5, label: "Sprunghöhe - 09.21"},
-    {value: 29.9, label: "Sprunghöhe - 06.22"},
-]
-const ref =    [{value: 0, label: ""}, {value: 40, label: "Referenzwert(cm)"}];
-
-const titel = "Profile Chart"
 export default class ProfileChart extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {arrtest: [], ref: [], title: ""};
-        this.getAll = this.getAll.bind(this);
     }
-
-
-    componentDidMount() {
-        this.getAll();
-
-    }
-    getAll(){
-        //need to add empty object for space
-        this.setState({ref: ref, arrtest: testdata, title: titel });
-        handelTrainer.getVideos().then(response => {
-            if(response.data.res === "error") {
-                const arr = ["connection error"];
-                this.setState({videoList: arr});
-                return;
-            }
-            if(response.data.res === "no"){
-                alert("Bitte erst anmelden.");
-                return;
-            }
-            if(response.data.res === "ok") {
-                var list = response.data.videoList.sort((a, b) => (a.time > b.time) ? -1 : 1);
-                this.setState({videoList: list});
-                this.updateVideoPage(list, this.state.currentPage);
-            }
-
-        }).catch(e => {
-            console.log(e);
-            alert("Es ist ein Fehler aufgetreten!");
-        });
-    }
-
-
-
-    
-
-
     render() {
         //require("./uploadFile.css")
-
+        const { arrtest, refer, title } = this.props;
         return (
             <div style= {{...{textAlign: 'center'}}}>
             <Plot
                 data={[
                     {
-                    x: this.state.arrtest.map(el => el.value),
-                    y: this.state.arrtest.map(el => el.label),
+                    x: arrtest.map(el => el.value),
+                    y: arrtest.map(el => el.label),
                     type: 'bar',
                     orientation: 'h',
                     mode: 'lines+markers',
                     marker: {color: '#862A16'},
-                    text: this.state.arrtest.map(el => el.value),
+                    text: arrtest.map(el => el.value),
                     },
                     {
-                        x: this.state.ref.map(el => el.value),
-                        y: this.state.ref.map(el => el.label),
+                        x: refer.map(el => el.value),
+                        y: refer.map(el => el.label),
                         type: 'bar',
                         orientation: 'h',
                         mode: 'lines+markers',
                         marker: {color: '#862A16'},
-                        text: this.state.ref.map(el => el.value),
+                        text: refer.map(el => el.value),
                         },
                     
                 ]}
-                layout={ { title: this.state.title, yaxis: { automargin: true}, grid: {rows: 2, columns: 1}, showlegend: false}}
+                layout={ { title: title, yaxis: { automargin: true}, grid: {rows: 2, columns: 1}, showlegend: false}}
                 />
                 
             </div>
