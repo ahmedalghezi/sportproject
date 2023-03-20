f70eb1a65a0997116c8e0b8441d575fccec36677
minisidi <83781799+minisidi@users.noreply.github.com>
2023-03-20 10:01:08 +0100

    upload file functionality for profile - in progress

--- src/vanessa/profile/profileCopy.js
+++ src/vanessa/profile/profileCopy.js
@@ -7,18 +7,45 @@ import React, { Component } from "react";
 import "./profile.css";
 import PostCSVData from "../../DB/postCSV";
 import LinkList from "../../user/LinkList";
+import Collapsible from "./Collapsible";
 
 //Profile Template
 
-class Profile2 extends Component {
+class ProfileC extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      filesList: [],
-      surveyList: [],
+      filesList: [
+        {
+          folder1: [
+            { name: "File 1", url: "https://example.com/folder1/file1" },
+            { name: "File 2", url: "https://example.com/folder1/file2" },
+            { name: "File 3", url: "https://example.com/folder1/file3" },
+          ],
+        },
+        {
+          folder2: [
+            { name: "File 1", url: "https://example.com/folder2/file1" },
+            { name: "File 2", url: "https://example.com/folder2/file2" },
+          ],
+        },
+      ],
+      surveyList: [
+        {
+          title: "Zu meinen Fragebögen 1",
+          link: "https://example.com/folder2/file1",
+        },
+        {
+          title: "Zu meinen Fragebögen 2",
+          link: "https://example.com/folder2/file2",
+        },
+      ],
       emptyMsg: "",
       showEmptyMsg: false,
       hideSurvey: false,
+      admin: false,
+      role: "",
+      isTrainer: false,
       links: [
         {
           folder1: [
@@ -51,6 +78,7 @@ class Profile2 extends Component {
           ],
         },
       ],
+      hideAllMsgs: true,
     };
   }
 
@@ -65,16 +93,21 @@ class Profile2 extends Component {
         if (response.data.res === "error") {
           alert("Es ist ein Fehler aufgetreten. Code pro 30");
           return;
-        } else if (
-          response.data.files &&
-          response.data.res === "ok"
-        ) {
+        } else if (response.data.filesP && response.data.res === "ok") {
           //this.setState({ filesList: response.data.files });
-          this.setState({ links: response.data.files });
+          this.setState({ links: response.data.filesP });
+          if (response.data.admin) {
+            this.setState({ admin: true });
+          }
+          this.setState({ role: response.data.role });
+          this.setState({ isTrainer: response.data.role === "trainer" });
         }
         if (response.data.empty) {
           this.setState({ showEmptyMsg: true });
         }
+        if (response.data.hideAllMsgs) {
+          this.setState({ hideAllMsgs: true });
+        } else this.setState({ hideAllMsgs: false });
         if (response.data.res === "no") {
           window.location.href =
             window.location.origin + "/reg/sign-in?org=$user$profile";
@@ -116,7 +149,7 @@ class Profile2 extends Component {
       "https://inprove-sport.info/" + "files/viewMyFiles/" + event.target.name;
   };
 
-  deleteFile(fileName) {
+  deleteFile = (fileName) => {
     PostCSVData.deleteFile({ fileName: fileName })
       .then((response) => {
         if (response.data.res === "error") {
@@ -134,7 +167,7 @@ class Profile2 extends Component {
         console.log(e);
         alert("Es ist ein Fehler aufgetreten. Code Pro93");
       });
-  }
+  };
 
   showEmptyMessage() {
     const msg =
@@ -169,63 +202,113 @@ class Profile2 extends Component {
               update profile
             </button>
           </div>
-          <div hidden={!this.state.showEmptyMsg}>
-            <p>Hallo,</p>
-            <p>willkommen im Projektportal des in:prove Projektes.</p>
-            <p>
-              Aktuell können wir Deine Ergebnisse noch nicht für den Abruf
-              bereitstellen. Wir geben Dir Bescheid, sobald Du diese im
-              Projektportal abrufen kannst.
-            </p>
-            <p>
-              Mit sportlichen Grüßen,<br></br> Dein in:prove Team
-            </p>
+          <div hidden={this.state.hideAllMsgs}>
+            <div hidden={!this.state.showEmptyMsg || this.state.isTrainer}>
+              <p>Hallo,</p>
+              <p>willkommen im Projektportal des in:prove Projektes.</p>
+              <p>
+                Aktuell können wir Deine Ergebnisse noch nicht für den Abruf
+                bereitstellen. Wir geben Dir Bescheid, sobald Du diese im
+                Projektportal abrufen kannst.
+              </p>
+              <p>
+                Mit sportlichen Grüßen,<br></br> Dein in:prove Team
+              </p>
+            </div>
+            <div hidden={this.state.showEmptyMsg || this.state.isTrainer}>
+              <p>Hallo!</p>
+              <p>
+                Hier in der Projektdatenbank findest Du die Rückmeldungen Deiner
+                durchgeführten Diagnostiken als auch alle weiteren relevanten
+                Projektinformationen. Zu den Rückmeldungen laden wir Dir auch
+                immer eine Verschwiegenheitserklärung hoch. Du darfst frei
+                bestimmen, mit wem Du Deine Daten teilen möchtest. Dies kann für
+                Dich von Vorteil sein, um so z.B. das Training oder die
+                Ernährung besser auf Deine Bedürfnisse anpassen zu können. Wenn
+                Du Deine Daten mit jemandem wie z. B. Deinen TrainerInnen,
+                ErnährungsberaterInnen, o.ä. teilen möchtest, ist es wichtig,
+                dass diese Person die Verschwiegenheitserklärung unterschreibt
+                und uns zusendet. So können wir sicherstellen, dass Deine Daten
+                geschützt sind.
+              </p>
+              <p>
+                {" "}
+                Solltest du Probleme oder Fragen bezüglich der Datenbank oder
+                Deiner Daten haben, melde dich gerne jederzeit bei uns. Du
+                erreichst uns unter fragen@inprove.info.
+              </p>
+              <p>
+                Mit sportlichen Grüßen,<br></br> Dein in:prove Team
+              </p>
+            </div>
+            <div hidden={!this.state.isTrainer}>
+              <p>Hallo!</p>
+              <p>
+                Hier in der in:prove-Projektdatenbank findest Du die
+                Rückmeldungen Deiner AthletInnen und alle weiteren relevanten
+                Projekt-Informationen. Alle Deine AthletInnen erhalten Ihre
+                individuellen Rückmeldungen ebenfalls über Ihr Profil in der
+                Datenbank, sodass Du diese nicht weiterleiten musst. Bitte
+                beachte, dass Du diese Daten nicht weiterleiten darfst. Deine
+                AthletInnen haben jedoch die Möglichkeit, ihre Rückmeldungen
+                nach Wunsch und Bedarf (z.B. an die HeimtrainerInnen)
+                weiterzuleiten. Hierfür haben sie eine
+                Verschwiegenheitserklärung erhalten. Jede Person, der die
+                Einsicht durch die AthletInnen gewährt wird, muss diese
+                Erklärung vorab unterzeichnen und an uns zurücksenden. So können
+                wir sicherstellen, dass die Daten optimal geschützt sind.
+              </p>
+              <p>
+                {" "}
+                Solltest Du oder Deine SportlerInnen Fragen haben, könnt Ihr
+                Euch jederzeit gerne bei uns melden. Ihr erreicht uns unter
+                fragen@inprove.info.
+              </p>
+              <p>
+                Mit sportlichen Grüßen,<br></br> Dein in:prove Team
+              </p>
+            </div>
           </div>
-          <div hidden={this.state.showEmptyMsg}>
-            <p>Hallo!</p>
-            <p>
-              Hier in der Projektdatenbank findest Du die Rückmeldungen Deiner
-              durchgeführten Diagnostiken als auch alle weiteren relevanten
-              Projektinformationen. Zu den Rückmeldungen laden wir Dir auch
-              immer eine Verschwiegenheitserklärung hoch. Du darfst frei
-              bestimmen, mit wem Du Deine Daten teilen möchtest. Dies kann für
-              Dich von Vorteil sein, um so z.B. das Training oder die Ernährung
-              besser auf Deine Bedürfnisse anpassen zu können. Wenn Du Deine
-              Daten mit jemandem wie z. B. Deinen TrainerInnen,
-              ErnährungsberaterInnen, o.ä. teilen möchtest, ist es wichtig, dass
-              diese Person die Verschwiegenheitserklärung unterschreibt und uns
-              zusendet. So können wir sicherstellen, dass Deine Daten geschützt
-              sind.
-            </p>
-            <p>
-              {" "}
-              Solltest du Probleme oder Fragen bezüglich der Datenbank oder
-              Deiner Daten haben, melde dich gerne jederzeit bei uns. Du
-              erreichst uns unter fragen@inprove.info.
-            </p>
-            <p>
-              Mit sportlichen Grüßen,<br></br> Dein in:prove Team
-            </p>
-          </div>
+
           <hr></hr>
 
-          <div id="beside">
+          <div id="collaps-beside">
             <div className="list-one" hidden={this.state.hideSurvey}>
-              <h4>Meine Umfragen</h4>
-
-              <ul>
-                <ul>
-                  {this.state.surveyList.map((item) => (
-                    <li key={item.title}>
-                      <a href={item.link}>{item.title}</a>
-                    </li>
-                  ))}
-                </ul>
-              </ul>
+              <Collapsible label="Meine Umfragen">
+                <div className="collapcontent">
+                  <ul>
+                    <ul>
+                      {this.state.surveyList.map((item) => (
+                        <li key={item.title}>
+                          <a href={item.link}>{item.title}</a>
+                        </li>
+                      ))}
+                    </ul>
+                  </ul>
+                </div>
+              </Collapsible>
             </div>
             <div className="list-two">
-              <h4>Meine Dateien</h4>
-              <LinkList links={this.state.links} />
+              <Collapsible label="Meine Dateien">
+                <div className="collapcontent">
+                  <LinkList
+                    links={this.state.links}
+                    admin={this.state.admin}
+                    onDeleteFile={this.deleteFile}
+                  />
+                </div>
+              </Collapsible>
+            </div>
+            <div className="list-three">
+              <Collapsible label="Uploads">
+                <div className="collapcontent">
+                  <LinkList
+                    links={this.state.filesList}
+                    admin={this.state.admin}
+                    onDeleteFile={this.deleteFile}
+                  />
+                </div>
+              </Collapsible>
             </div>
           </div>
         </div>
@@ -234,4 +317,4 @@ class Profile2 extends Component {
   }
 }
 
-export default Profile2;
+export default ProfileC;
