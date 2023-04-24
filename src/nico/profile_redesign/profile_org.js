bceaf6dd6a662a9dca80c3623f98637828675c02
Nicolas Schulz <nicolas.schulz@web.de>
2023-04-24 14:20:41 +0200

    added profile.js changes

--- src/nico/profile_redesign/profile_org.js
+++ src/nico/profile_redesign/profile_org.js
@@ -19,6 +19,7 @@ import Modal from '@mui/material/Modal';
 import LoadCharts from "../profilechart/profile_charts";
 import graphimage from "./graph.png"
 import avaimg from "./avatar.png"
+import LinkList from "../../user/LinkList";
 
 //Profile Template
 const diastyle = {
@@ -78,6 +79,11 @@ class TestProfileC extends Component {
       showSurvey: false,
       showFiles: false,
       showReports: false,
+      admin: false,
+      role: "",
+      isTrainer: false,
+      links: [],
+      hideAllMsgs: true,
     };
   }
 
@@ -86,6 +92,7 @@ class TestProfileC extends Component {
   componentDidMount() {
     this.getFiles();
     this.getSurveys();
+    this.getMyOwnFiles();
   }
 
   getFiles = () => {
@@ -129,6 +136,40 @@ class TestProfileC extends Component {
         .catch((e) => {
           console.log(e);
           alert("Es ist ein Fehler aufgetreten. Code Pro55");
+        });
+  };
+
+  getMyOwnFiles = () => {
+    PostCSVData.getMyOwnFiles()
+        .then((response) => {
+          if (response.data.res === "error") {
+            alert("Es ist ein Fehler aufgetreten. Code pro 30");
+            return;
+          } else if (response.data.filesP && response.data.res === "ok") {
+            //this.setState({ filesList: response.data.files });
+            this.setState({ filesList: response.data.filesP });
+            if (response.data.admin) {
+              this.setState({ admin: true });
+            }
+            this.setState({ role: response.data.role });
+            this.setState({ isTrainer: response.data.role === "trainer" });
+          }
+          if (response.data.empty) {
+            this.setState({ showEmptyMsg: true });
+          }
+          if (response.data.hideAllMsgs) {
+            this.setState({ hideAllMsgs: true });
+          } else this.setState({ hideAllMsgs: false });
+          if (response.data.res === "no") {
+            window.location.href =
+                window.location.origin + "/reg/sign-in?org=$user$profile";
+            return;
+            //xxx alert("Bitte melde Dich an.");
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+          alert("Es ist ein Fehler aufgetreten. Code Pro39");
         });
   };
 
@@ -259,10 +300,74 @@ class TestProfileC extends Component {
               Willkommen bei in:prove!
         </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        <p>Hallo,</p><p>willkommen im Projektportal des in:prove Projektes.</p><p>Aktuell können wir
-              Deine Ergebnisse noch nicht für den Abruf bereitstellen.
-              Wir geben Dir Bescheid, sobald Du diese im Projektportal abrufen kannst.</p>
-              <p>Mit sportlichen Grüßen,<br></br> Dein in:prove Team</p>
+        <div hidden={this.state.hideAllMsgs}>
+              <div hidden={!this.state.showEmptyMsg || this.state.isTrainer}>
+                <p>Hallo,</p>
+                <p>willkommen im Projektportal des in:prove Projektes.</p>
+                <p>
+                  Aktuell können wir Deine Ergebnisse noch nicht für den Abruf
+                  bereitstellen. Wir geben Dir Bescheid, sobald Du diese im
+                  Projektportal abrufen kannst.
+                </p>
+                <p>
+                  Mit sportlichen Grüßen,<br></br> Dein in:prove Team
+                </p>
+              </div>
+              <div hidden={this.state.showEmptyMsg || this.state.isTrainer}>
+                <p>Hallo!</p>
+                <p>
+                  Hier in der Projektdatenbank findest Du die Rückmeldungen Deiner
+                  durchgeführten Diagnostiken als auch alle weiteren relevanten
+                  Projektinformationen. Zu den Rückmeldungen laden wir Dir auch
+                  immer eine Verschwiegenheitserklärung hoch. Du darfst frei
+                  bestimmen, mit wem Du Deine Daten teilen möchtest. Dies kann für
+                  Dich von Vorteil sein, um so z.B. das Training oder die
+                  Ernährung besser auf Deine Bedürfnisse anpassen zu können. Wenn
+                  Du Deine Daten mit jemandem wie z. B. Deinen TrainerInnen,
+                  ErnährungsberaterInnen, o.ä. teilen möchtest, ist es wichtig,
+                  dass diese Person die Verschwiegenheitserklärung unterschreibt
+                  und uns zusendet. So können wir sicherstellen, dass Deine Daten
+                  geschützt sind.
+                </p>
+                <p>
+                  {" "}
+                  Solltest du Probleme oder Fragen bezüglich der Datenbank oder
+                  Deiner Daten haben, melde dich gerne jederzeit bei uns. Du
+                  erreichst uns unter fragen@inprove.info.
+                </p>
+                <p>
+                  Mit sportlichen Grüßen,<br></br> Dein in:prove Team
+                </p>
+              </div>
+              <div hidden={!this.state.isTrainer}>
+                <p>Hallo!</p>
+                <p>
+                  Hier in der in:prove-Projektdatenbank findest Du die
+                  Rückmeldungen Deiner AthletInnen und alle weiteren relevanten
+                  Projekt-Informationen. Alle Deine AthletInnen erhalten Ihre
+                  individuellen Rückmeldungen ebenfalls über Ihr Profil in der
+                  Datenbank, sodass Du diese nicht weiterleiten musst. Bitte
+                  beachte, dass Du diese Daten nicht weiterleiten darfst. Deine
+                  AthletInnen haben jedoch die Möglichkeit, ihre Rückmeldungen
+                  nach Wunsch und Bedarf (z.B. an die HeimtrainerInnen)
+                  weiterzuleiten. Hierfür haben sie eine
+                  Verschwiegenheitserklärung erhalten. Jede Person, der die
+                  Einsicht durch die AthletInnen gewährt wird, muss diese
+                  Erklärung vorab unterzeichnen und an uns zurücksenden. So können
+                  wir sicherstellen, dass die Daten optimal geschützt sind.
+                </p>
+                <p>
+                  {" "}
+                  Solltest Du oder Deine SportlerInnen Fragen haben, könnt Ihr
+                  Euch jederzeit gerne bei uns melden. Ihr erreicht uns unter
+                  fragen@inprove.info.
+                </p>
+                <p>
+                  Mit sportlichen Grüßen,<br></br> Dein in:prove Team
+                </p>
+              </div>
+            </div>
+      
         </Typography>
           </Box>
         </Modal>
@@ -298,17 +403,14 @@ class TestProfileC extends Component {
         >
           <Box sx={standardstyle}>
               <Typography id="modal-modal-title" variant="h6" component="h2">
-              Meine Berichte
+              Meine Dateien
         </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        <ul>
-                  {this.state.filesList.map((item) => (
-                      <li key={item.file_name} onClick={this.handleFileClick}>
-                        <a hidden={!item.admin} name={"deletevftr5"+item.file_name} href={"#" + item.file_name}>(delete) </a>
-                        <a name={item.file_name} href={"#" + item.file_name}>{item.title}</a>
-                      </li>
-                  ))}
-                </ul>
+        <LinkList
+                    links={this.state.links}
+                    admin={this.state.admin}
+                    onDeleteFile={this.deleteFile}
+                />
         </Typography>
           </Box>
         </Modal>
@@ -321,10 +423,14 @@ class TestProfileC extends Component {
         >
           <Box sx={standardstyle}>
               <Typography id="modal-modal-title" variant="h6" component="h2">
-              Meine Dateien
+              Uploads
         </Typography>
         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-        
+        <LinkList
+                    links={this.state.filesList}
+                    admin={this.state.admin}
+                    onDeleteFile={this.deleteMyFile}
+                />
         </Typography>
           </Box>
         </Modal>
@@ -372,7 +478,7 @@ class TestProfileC extends Component {
         fontWeight="bold"
         sx={{ m: "0 0 5px 0" }}
       >
-        Meine Berichte
+        Meine Dateien
       </Typography>
           <SummarizeIcon
                 sx={{ color: "#535ac8", fontSize: "36px" }}
@@ -392,7 +498,7 @@ class TestProfileC extends Component {
         fontWeight="bold"
         sx={{ m: "0 0 5px 0" }}
       >
-        Meine Dateien
+        Uploads
       </Typography>
           <UploadFileIcon
                 sx={{ color: "#535ac8", fontSize: "36px" }}
