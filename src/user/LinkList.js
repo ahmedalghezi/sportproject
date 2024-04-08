d0d35ea882f3db4c13764f02bfe084bbb06e8ef2
prernt <44579002+prernt@users.noreply.github.com>
2024-04-08 17:34:01 +0200

    Update LinkList.js

--- src/user/LinkList.js
+++ src/user/LinkList.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+const regex = /(\d+)_/;
+
 const Link = ({ name, url }) => (
     <a href={url} target="_blank" rel="noopener noreferrer">{name}</a>
 );
@@ -21,17 +23,38 @@ const Folder = ({ folder ,admin,onDeleteFile}) => {
     return (
         <div>
             <button className={"form-control"} onClick={toggleExpanded}>{isExpanded ? '- ' : '+ '}{Object.keys(folder)[0]}</button>
-
             {isExpanded && (
                 <ul>
-                    {folder[Object.keys(folder)[0]].map((link, index) => (
-                        <li key={link.url} onClick={handleFileClick}>
-                            {/*<Link name={"deletevftr5"+link.name} url={"#" +link.url} />*/}
-                            {/* title={link.name} */}
-                            <a hidden={!admin} name={"deletevftr5"+link.url} href={"#" + link.url} title={link.name}>(delete) </a>
-                            <a name={link.url} href={"#" + link.url} title={link.name}>{link.name}</a>
-                        </li>
-                    ))}
+                    {folder[Object.keys(folder)[0]].map((link, index) => {
+                        const match = link.name.match(regex)
+                        const shouldPerformMatch = match && match[1].length >= 12 && match[1].length <= 15;
+
+                        return (
+                            <li key={link.url} onClick={handleFileClick}>
+                                {/* <Link name={"deletevftr5"+link.name} url={"#" +link.url} /> */}
+                                {/* title={link.name} */}
+                                <a hidden={!admin} name={"deletevftr5" + link.url} href={"#" + link.url} title={link.name}>
+                                    (delete) 
+                                </a>
+                                <a
+                                    name={link.url}
+                                    href={"#" + link.url}
+                                    title={link.name}
+                                    // Conditionally remove regex match from link.name
+                                    onClick={() => shouldPerformMatch && handleFileClick(link.name.replace(regex, ''))}
+                                >
+                                    {shouldPerformMatch ? link.name.replace(regex, '') : link.name}
+                                </a>
+                            </li>
+                        );
+            
+                        // <li key={link.url} onClick={handleFileClick}>
+                        //     {/*<Link name={"deletevftr5"+link.name} url={"#" +link.url} />*/}
+                        //     {/* title={link.name} */}
+                        //     <a hidden={!admin} name={"deletevftr5"+link.url} href={"#" + link.url} title={link.name}>(delete) </a>
+                        //     <a name={link.url} href={"#" + link.url} title={link.name}>{link.name}</a>
+                        // </li>
+                })}
                 </ul>
             )}
         </div>
